8fd9f162f1f4745aafd930b29b5df96364fb9875
Chen-Anthony <jia.chen4@ontariotechu.net>
2022-10-03 12:51:28 -0400

    About me page
    Created the basic layout for about me page

--- navbar.js
+++ navbar.js
@@ -1,11 +1,9 @@
 // These are getting the links and the logo thats being clicked
-const logo2 = document.getElementsByClassName("logo_2")[0];
-const navbarlinks = document.getElementsByClassName("navigation_links")[0];
+const logo_2 = document.getElementsByClassName("logo_2")[0];
+const navigation_links = document.getElementsByClassName("navigation_links")[0];
 // this waits for the click so that it can perform being active and hid/reveal the list
-logo2.addEventListener("click", () => {
-  navbarlinks.classList.toggle("active");
-})
+logo_2.addEventListener("click", () => {
+  navigation_links.classList.toggle("active");
+});
 
 
-
-  
